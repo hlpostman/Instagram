366af7a2ada4acc46dd5ec27302a73f7fadca966
Hannah Lily Postman <postman.hl@gmail.com>
2017-03-09 17:52:13 -0800

    Add table view to timeline

--- Instagram/TimelineViewController.swift
+++ Instagram/TimelineViewController.swift
@@ -11,6 +11,9 @@ import Parse
 
 class TimelineViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
